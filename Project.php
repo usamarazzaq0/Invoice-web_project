3d9ee9d56f0e7d15a6edd2e8b182523f4255a02e
usamarazzaq0 <160162848+usamarazzaq0@users.noreply.github.com>
2024-06-21 23:24:32 +0100

    Add files via upload

---
+++ Project.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+/**
+ * App\Models\Project
+ *
+ * @property-read Classroom $classroom
+ * @property-read MediaCollection|Media[] $media
+ * @property-read int|null $media_count
+ * @property-read Collection|ProjectUser[] $projects
+ * @property-read int|null $projects_count
+ * @method static Builder|Project newModelQuery()
+ * @method static Builder|Project newQuery()
+ * @method static Builder|Project query()
+ * @property int $id
+ * @property int $classroom_id
+ * @property string $name
+ * @property int $marks
+ * @property string $description
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Project whereClassroomId($value)
+ * @method static Builder|Project whereCreatedAt($value)
+ * @method static Builder|Project whereDescription($value)
+ * @method static Builder|Project whereId($value)
+ * @method static Builder|Project whereMarks($value)
+ * @method static Builder|Project whereName($value)
+ * @method static Builder|Project whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
+class Project extends Model implements HasMedia
+{
+    use HasFactory, InteractsWithMedia;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'classroom_id',
+        'name',
+        'marks',
+        'description',
+    ];
+
+    public function classroom(): BelongsTo
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+
+    public function projects(): HasMany
+    {
+        return $this->hasMany(ProjectUser::class);
+    }
+}
